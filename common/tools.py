0b25e9d29cf3dc5ad1f9f7a6a6dbf98bae0955a3
2020668 <keen2020@outlook.com>
2019-08-13 13:15:06 +0800

    Initial commit

---
+++ common/tools.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+
+=================================
+Author: keen
+Created on: 2019/6/25
+
+E-mail:keen2020@outlook.com
+
+=================================
+
+
+"""
+
+import re
+from common.config import conf
+import random
+
+# 匹配两个#中间的任意字符，至少一次，关闭贪婪
+p = r"#(.+?)#"
+
+
+class ConText(object):
+    pass
+
+
+# def read(data):
+#     setattr(ConText, "key", data)
+
+
+def data_replace(data):
+    """
+    :param data: 用例的参数
+    :return: 替换之后的结果
+    """
+    while re.search(p, data):
+        key = re.search(p, data).group(1)
+        try:
+            value = conf.get("test_data", key)
+        except:
+            # value = getattr(ConText, "loanid")
+            if "loanid" in data:
+                value = getattr(ConText, "loanid")
+        data = re.sub(p, value, data, count=1)
+    return data
+
+
+def rand_phone():
+    phone = "133"
+    for i in range(8):
+        phone_end = random.randint(0, 9)
+        phone += str(phone_end)
+    return phone
+
+
+if __name__ == '__main__':
+    data = "hasdgiioghgaoigh#login_phone1#shg;g#pwd#"
+    res = data_replace(data)
+    print(res)
---
+++ run_suite.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+
+=================================
+Author: keen
+Created on: 2019/6/4
+
+E-mail:keen2020@outlook.com
+
+=================================
+
+
+"""
+
+import unittest
+from library.HTMLTestRunnerNew import HTMLTestRunner
+from common.config import conf
+from common.constant import CASE_DIR, REPORT_DIR
+from common.send_email import SendEmail
+import os
+import time
+
+
+_title = conf.get('report', 'title')
+_description = conf.get('report', 'description')
+_tester = conf.get('report', 'tester')
+report_name = conf.get('report', 'report_name')
+report_name = time.strftime("%Y%m%d%H%M%S", time.localtime()) + "_" + report_name
+
+ 
+suite = unittest.TestSuite()  # 创建测试集合
+loader = unittest.TestLoader()
+suite.addTest(loader.discover(CASE_DIR))
+
+file_path = os.path.join(REPORT_DIR, report_name)
+
+with open(file_path, 'wb') as f:
+    runner = HTMLTestRunner(
+        stream=f,
+        verbosity=2,
+        title=_title,
+        description=_description,
+        tester=_tester
+    )
+    runner.run(suite)
+
+
+mail_title = "亿钱贷项目接口测试报告"
+mail_message = "这是亿钱贷接口测试报告，请各位领导注意查收，谢谢!"
+SendEmail.send_qq_file_mail(mail_title, mail_message, file_path)
+# SendEmail.send_outlook_file_mail(mail_title, mail_message, file_path)
